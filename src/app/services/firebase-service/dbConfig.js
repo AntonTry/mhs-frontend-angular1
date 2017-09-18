c9ed5c5ca5935aadd2d36b39be298db677d7f799
Antony <antontr@interlink-ua.com>
2017-09-18 11:05:16 +0300

    Add dbConfig on deploy

---
+++ src/app/services/firebase-service/dbConfig.js
@@ -0,0 +1,9 @@
+let config = {
+  apiKey: "AIzaSyDaYN78sZJpVnNdVx8_wlhNzl5xfv7gCqM",
+  authDomain: "testproj-cd085.firebaseapp.com",
+  databaseURL: "https://testproj-cd085.firebaseio.com",
+  projectId: "testproj-cd085",
+  storageBucket: "testproj-cd085.appspot.com",
+  messagingSenderId: "743525828232"
+};
+firebase.initializeApp(config);
